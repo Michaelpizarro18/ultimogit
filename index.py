649ed711081ad7e5491ee2611a5ea93a24f98c2a
Michaelpizarro18 <mpizarrota@ucvvirtual.edu.pe>
2024-09-08 12:34:30 -0500

    Project Start

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='App')
+
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route('/cuerpo')
+def cuerpo():
+    return render_template('cuerpo.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
