81ec57f9dbb607176c379a7eeffadc68d12590b1
Michaelpizarro18 <mpizarrota@ucvvirtual.edu.pe>
2024-09-09 17:10:23 -0500

    Project Start

--- index.py
+++ index.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template
-from flask_frozen import Freezer
 
-# Crea la aplicación Flask
 app = Flask(__name__, template_folder='App')
 
-# Crea un objeto Freezer
-freezer = Freezer(app)
-
-# Definición de rutas
 @app.route('/')
 def principal():
     return render_template('index.html')
@@ -16,6 +10,6 @@ def principal():
 def cuerpo():
     return render_template('cuerpo.html')
 
-# Ejecuta la aplicación en modo de desarrollo
+
 if __name__ == '__main__':
     app.run(debug=True)
