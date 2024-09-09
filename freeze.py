63fe7e361eb7772ee919215202d600a94c431a5b
Michaelpizarro18 <mpizarrota@ucvvirtual.edu.pe>
2024-09-09 17:34:13 -0500

    Project Start

--- freeze.py
+++
@@ -1,4 +0,0 @@
-from index import freezer  # Importa 'freezer' desde tu archivo principal donde está la aplicación Flask
-
-if __name__ == '__main__':
-    freezer.freeze()  # Llama a 'freeze()' para generar los archivos HTML estáticos
